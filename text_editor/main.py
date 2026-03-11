93b575596f97cc01c9b87c8f6ebf244fb4ff66df
minecraftfighter <barthaliscsanad@gmail.com>
2026-03-11 19:57:43 +0200

    Update main.py

--- text_editor/main.py
+++ text_editor/main.py
@@ -1,17 +1,17 @@
 import tkinter as tk
 from tkinter import messagebox
-import os
+from tkinter import filedialog
 
 root = tk.Tk()
 root.geometry("500x400")
-root.title("python-TextEditor")
+root.title("PyPad")
 root.call('source', 'Azure/azure.tcl')
 root.call('set_theme', 'dark')
 filename = ""
 
 def load_file():
 
-    filename = fne.get(1.0, 'end-1c')
+    filename = filedialog.askopenfilename()
     f2 = open(filename, 'r')
     dat = f2.read()
     out.delete(1.0, "end-1c")
@@ -19,21 +19,20 @@ def load_file():
 
 def save():
 
-    filename = fne.get(1.0, 'end-1c')
+    filename = filedialog.asksaveasfilename(filetypes=(("Text Document", "*.txt"), ("All Files", "*.*")))
     tw = out.get('1.0', 'end-1c')
     file = open(filename, 'w')
     file.write(tw)
     file.close()
 
 def bl():
-
     root.call('set_theme', 'dark')
 
 def li():
     root.call('set_theme', 'light')
 
 def msg():
-    messagebox.showinfo("HELP", "Enter the filename to the first box, and enter the information to the second box.")
+    messagebox.showinfo("HELP", "Enter the things you want to save.")
 
 
 menuu = tk.Menu(root)
@@ -51,19 +50,10 @@ vis_men.add_command(label="Dark", command=bl)
 vis_men.add_command(label="Light", command=li)
 he_men.add_command(label="Help", command=msg)
 
-label = tk.Label(root, text="Python-TextEditor", font=('Arial', 15))
+label = tk.Label(root, text="PyPad", font=('Arial', 15))
 label.pack(padx=10, pady=10)
 
-lab1 = tk.Label(root, text="Filename", font=('Arial', 15))
-lab1.pack(padx=5, pady=5)
-
-fne = tk.Text(root,height=1, font=('Arial', 15))
-fne.pack(padx=12, pady=12)
-
-lab2 = tk.Label(root, text="Input", font=('Arial', 15))
-lab2.pack(padx=5, pady=5)
-
-out = tk.Text(root, height=4, font=('Arial', 15))
+out = tk.Text(root, height=6, font=('Arial', 15))
 out.pack(padx=12, pady=12)
 
 root.mainloop()
